fde4d802ef121de010a8ef44a2fc2393bf9b3246
Feyisayo Badmus <feymus99@gmail.com>
2025-06-15 01:23:12 +0300

    fix: Prisma Client import

--- prisma/generated/zod/index.ts
+++ prisma/generated/zod/index.ts
@@ -1,5 +1,5 @@
 import { z } from 'zod';
-import { Prisma } from '@prisma/client';
+import { Prisma } from '../../../node_modules/.prisma/client';
 
 /////////////////////////////////////////
 // HELPER FUNCTIONS
